546dfca8c4e745868df221cf97d8fe33e9c52fcb
toki-0625 <tokiaya0625@gmail.com>
2023-11-16 23:18:18 +0900

    lesson5課題

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,25 @@
+package baseball;
+
+public class BaseBallTeam {
+        //フィールド
+        protected String name;  //球団名
+        protected int win;      //勝利数
+        protected int lose;     //敗北数
+        protected int drow;     //引分数
+    //引数を用いるコンストラクタ
+    public BaseBallTeam(String name, int win, int lose, int drow) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.drow = drow;
+    }
+    //勝率を求めるメソッド
+    public double getRate() {
+       double rate = (double) win / (win + lose);
+       return rate;
+    }
+    //reportを表示するメソッド
+    public void report() {
+        System.out.println(this.name + "の2022年の成績は" + this.win + "勝" +  this.lose + "敗" + this.drow + "分、" + "勝率は" + getRate());
+    }
+}     
